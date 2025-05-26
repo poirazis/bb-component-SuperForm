e6fcfa2b07e5d7b161039d0a769565c2206045f3
poirazis <poirazis@gmail.com>
2025-05-26 17:38:56 +0300

    Release 1.0.0

---
+++ src/Component.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { getContext } from "svelte";
+  import { SuperForm } from "@poirazis/supercomponents-shared";
+
+  export let text;
+
+  const { styleable } = getContext("sdk");
+  const component = getContext("component");
+</script>
+
+<div use:styleable={$component.styles}>
+  <SuperForm {...$$props}>
+    <slot />
+  </SuperForm>
+</div>
