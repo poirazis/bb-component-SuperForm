4a6a1b2725b9a01fb4eb694ac041cab2a854d3fa
poirazis <poirazis@gmail.com>
2025-05-29 15:34:35 +0300

    v1.0.2

--- src/Component.svelte
+++ src/Component.svelte
@@ -2,14 +2,27 @@
   import { getContext } from "svelte";
   import { SuperForm } from "@poirazis/supercomponents-shared";
 
-  export let text;
+  export let flex;
+  export let onChange;
+  export let onReset;
 
-  const { styleable } = getContext("sdk");
+  const { styleable, memo } = getContext("sdk");
   const component = getContext("component");
+
+  let props = memo($$props);
+  $: props.set($$props);
+
+  $: $component.styles = {
+    ...$component.styles,
+    normal: {
+      ...$component.styles.normal,
+      flex,
+    },
+  };
 </script>
 
-<div use:styleable={$component.styles}>
-  <SuperForm {...$$props}>
+<div use:styleable={$component.styles} class="bb-component-SuperFormSimple">
+  <SuperForm {...$props} on:change={onChange} on:reset={onReset}>
     <slot />
   </SuperForm>
 </div>
